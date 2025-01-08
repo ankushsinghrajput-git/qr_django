71b6db6717106189040026181209533068404c93
ankushsinghrajput.main@gmail.com <ankushsinghrajput-git>
2025-01-09 02:24:47 +0530

    phase 01 completetion

---
+++ qr_generator/qr_generator/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+
+class QRCodeForm(forms.Form):
+    name = forms.CharField(
+        max_length=50,
+        label="Name",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Enter the title of QR"}
+        ),
+    )
+    url = forms.URLField(
+        max_length=200,
+        label="URL",
+        widget=forms.URLInput(
+            attrs={"class": "form-control", "placeholder": "Enter ur URL"}
+        ),
+    )
---
+++ qr_generator/qr_generator/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from .forms import QRCodeForm
+import qrcode
+import os
+from django.conf import settings
+
+
+def generate_qr_code(request):
+    if request.method == "POST":
+        form = QRCodeForm(request.POST)
+        if form.is_valid():
+            qr_name = form.cleaned_data["name"]
+            qr_url = form.cleaned_data["url"]
+
+            # Generate QR Code
+            qr = qrcode.make(qr_url)
+            filename = qr_name.replace(" ", "_").lower() + "_qr.png"
+            filepath = os.path.join(settings.MEDIA_ROOT, filename)
+            qr.save(filepath)
+
+            # QR URL
+            generated_qr_url = os.path.join(settings.MEDIA_URL, filename)
+
+            context = {"qr_name": qr_name, "generated_qr_url": generated_qr_url}
+            return render(request, "qr_result.html", context)
+
+    else:
+        form = QRCodeForm()
+        context = {
+            "form": form,
+        }
+        return render(request, "generate_qr_code.html", context)
